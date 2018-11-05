ff9cdd262eb50db92fed6f0f596a9ffc940cd10c
Rafael Plugge <rafael.plugge@email.com>
2018-11-05 13:07:00 +0000

    Initial commit and added Test Harness.

---
+++ GamesEngineeringPractical05/TestHarness/src/Test.cpp
@@ -0,0 +1,28 @@
+﻿#include "Test.h"
+
+void app::Test::checkForSixNumbers(std::initializer_list<int> const & numbers)
+{
+	assert(numbers.size() == 6);
+}
+
+void app::Test::checkNumbersAreBetweenRange(std::initializer_list<int> const & numbers)
+{
+	constexpr auto min = 1;
+	constexpr auto max = 46;
+	for (auto const & num : numbers)
+	{
+		assert(min < num && num < max);
+	}
+}
+
+void app::Test::checkNoNumbersAreRepeated(std::initializer_list<int> const & numbers)
+{
+	for (auto const & num1 : numbers)
+	{
+		for (auto const & num2 : numbers)
+		{
+			if (num1 == num2) { continue; }
+			assert(num1 != num2);
+		}
+	}
+}
---
+++ GamesEngineeringPractical05/TestHarness/src/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <cstdlib>
+#include <cassert>
+#include <array>
+
+#include "Test.h"
+
+int main(int argc, char** argv)
+{
+	// Success case
+	std::initializer_list<int> t2 = { 1, 2, 3, 4, 5, 6 };
+	app::Test::checkForSixNumbers(t2);
+	app::Test::checkNumbersAreBetweenRange(t2);
+	app::Test::checkNoNumbersAreRepeated(t2);
+
+	// Fail case
+	std::initializer_list<int> t1 = { 0, 2, 4, 70, 30, 20, 10, 20, 23, 12 };
+	app::Test::checkForSixNumbers(t1);
+	app::Test::checkNumbersAreBetweenRange(t1);
+	app::Test::checkNoNumbersAreRepeated(t1);
+
+	return EXIT_SUCCESS;
+}
