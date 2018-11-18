54d87dc6999c2607d6b56093ecfcda7f47185475
Rafael Plugge <rafael.plugge@email.com>
2018-11-19 01:10:14 +0202

    Fixed tests

--- GamesEngineeringPractical05/TestHarness/src/main.cpp
+++ GamesEngineeringPractical05/TestHarness/src/main.cpp
@@ -2,21 +2,50 @@
 #include <cassert>
 #include <array>
 
-#include "Test.h"
+#include "../../Application/src/Console.h"
 
 int main(int argc, char** argv)
 {
 	// Success case
-	std::initializer_list<int> t2 = { 1, 2, 3, 4, 5, 6 };
-	app::Test::checkForSixNumbers(t2);
-	app::Test::checkNumbersAreBetweenRange(t2);
-	app::Test::checkNoNumbersAreRepeated(t2);
-
+	{
+		std::initializer_list<int> t2 = { 1, 2, 3, 4, 5, 6 };
+		{
+			auto console = app::Console();
+			auto result = console.checkForSixNumbers(t2);
+			assert(result == true);
+		}
+		{
+			auto console = app::Console();
+			auto results = console.checkNumbersAreBetweenRange(t2);
+			// Check that there are no false booleans in results
+			assert(std::find_if(results.begin(), results.end(), [](bool const & result) { return result != false; }) != results.end());
+		}
+		{
+			auto console = app::Console();
+			auto results = console.checkNoNumbersAreRepeated(t2);
+			assert(std::find_if(results.begin(), results.end(), [](bool const & result) { return result != false; }) != results.end());
+		}
+	}
 	// Fail case
-	std::initializer_list<int> t1 = { 0, 2, 4, 70, 30, 20, 10, 20, 23, 12 };
-	app::Test::checkForSixNumbers(t1);
-	app::Test::checkNumbersAreBetweenRange(t1);
-	app::Test::checkNoNumbersAreRepeated(t1);
+	{
+		std::initializer_list<int> t1 = { 0, 2, 4, 70, 30, 20, 10, 20, 23, 12 };
+		{
+			auto console = app::Console();
+			auto result = console.checkForSixNumbers(t1);
+			assert(result == false);
+		}
+		{
+			auto console = app::Console();
+			auto results = console.checkNumbersAreBetweenRange(t1);
+			// Check that there is at least one false boolean in results
+			assert(std::find_if(results.begin(), results.end(), [](bool const & result) { return result == false; }) != results.end());
+		}
+		{
+			auto console = app::Console();
+			auto results = console.checkNoNumbersAreRepeated(t1);
+			assert(std::find_if(results.begin(), results.end(), [](bool const & result) { return result == false; }) != results.end());
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
