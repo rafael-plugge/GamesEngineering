9f213d5a3364ce4f3a2b160cfaf01246de617af8
Rafael Plugge <rafael.plugge@email.com>
2019-03-09 20:50:20 +0130

    Completed Tie Breaker Algorithm

---
+++ GamesEngineeringPractical07/Project/src/main.cpp
@@ -0,0 +1,41 @@
+﻿#include "stdafx.hpp"
+#include "utilities/Console.hpp"
+#include "utilities/Random.hpp"
+
+bool inOne = false,
+	inTwo = false;
+int last = 1;
+
+void csThreadOne()
+{
+	while (true)
+	{
+		inOne = true; last = 1;
+		while (inTwo && last == 1) continue;
+		// critical section
+		app::cout::wl("Thread[1] critical section");
+		inOne = false;
+	}
+}
+
+void csThreadTwo()
+{
+	while (true)
+	{
+		inTwo = true; last = 2;
+		while (inOne && last == 2) continue;
+		// critical section
+		app::cout::wl("Thread[2] critical section");
+		inTwo = false;
+	}
+}
+
+int main(int argc, char const ** argv)
+{
+	auto csOne = std::thread(csThreadOne);
+	auto csTwo = std::thread(csThreadTwo);
+	csOne.join();
+	csTwo.join();
+	std::cin.get();
+	return EXIT_SUCCESS;
+}
---
+++ GamesEngineeringPractical07/Project/src/utilities/Console.hpp
@@ -0,0 +1,153 @@
+﻿#pragma once
+
+#if !defined(_USE_STDAFX_HPP) && !defined(_USE_STDAFX_H)
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <variant>
+
+#endif // !defined(_USE_STDAFX_HPP) && !defined(_USE_STDAFX_H)
+
+#include "Variant.hpp"
+
+namespace app
+{
+	/// 
+	/// @brief Console class with static functions to write to console window.
+	/// @author Rafael Plugge
+	/// 
+	/// Contains static functions that make debug messages easier to write.
+	/// Automatically stops outputting to console if not in debug mode.
+	/// 
+	class Console
+	{
+	public: // Usings/Typedefs/enums
+		using Variant = std::variant<
+			  std::string
+			, std::uint8_t
+			, std::uint16_t
+			, std::uint32_t
+			, std::uint64_t
+			, std::int8_t
+			, std::int16_t
+			, std::int32_t
+			, std::int64_t
+			, std::float_t
+			, std::double_t
+		>;
+	public: // Constructors/Destructor/Assignments
+	public: // Public Static Functions
+		#ifdef _DEBUG
+
+		/// <summary>
+		/// Output to the console the passed text.
+		/// 
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		/// <param name="text">read-only reference to the text that will be output'd to console.</param>
+		static inline void write(Variant const & text)
+		{
+			std::visit(app::util::overload{
+				  [](std::string const & txt) { std::cout << txt; }
+				, [](auto const & num) { std::cout << std::to_string(num); }
+			}, text);
+		}
+		static inline void w(Variant const & text) { write(text); }
+
+		/// <summary>
+		/// Output to the console the collection of text passed.
+		/// 
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		/// <param name="texts">read-only reference to the collection of text that will be output'd to console.</param>
+		static inline void write(std::initializer_list<Variant> const & texts) { for (auto const & text : texts) { write(text); } }
+		static inline void w(std::initializer_list<Variant> const & texts) { write(texts); }
+
+		/// <summary>
+		/// Output to the console a empty new line.
+		///
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		static inline void writeLine() { std::cout << std::endl; }
+		static inline void wl() { writeLine(); }
+
+		/// <summary>
+		/// Output to the console the passed text, ending with a new line.
+		/// 
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		/// <param name="text">read-only refernece to the text that will be output'd to console.</param>
+		static inline void writeLine(Variant const & text) { write(text); std::cout << std::endl; }
+		static inline void wl(Variant const & text) { writeLine(text); }
+
+		/// <summary>
+		/// Output to the console the collection of text, after which adds a new line.
+		/// 
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		/// <param name="texts">read-only reference to the collection of text that will be output'd to console.</param>
+		static inline void writeLine(std::initializer_list<Variant> const & texts) { for (auto const & text : texts) { write(text); } std::cout << std::endl; }
+		static inline void wl(std::initializer_list<Variant> const & texts) { writeLine(texts); }
+
+		#else
+
+		/// <summary>
+		/// Output to the console the passed text.
+		/// 
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		/// <param name="text">read-only reference to the text that will be output'd to console.</param>
+		static inline void write(Variant const & text) {}
+		static inline void w(Variant const & text) { write(text); }
+
+		/// <summary>
+		/// Output to the console the collection of text passed.
+		/// 
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		/// <param name="texts">read-only reference to the collection of text that will be output'd to console.</param>
+		static inline void write(std::initializer_list<Variant> const & texts) {}
+		static inline void w(std::initializer_list<Variant> const & texts) { write(texts); }
+
+		/// <summary>
+		/// Output to the console a empty new line.
+		///
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		static inline void writeLine() {}
+		static inline void wl() { writeLine(); }
+
+		/// <summary>
+		/// Output to the console the passed text, ending with a new line.
+		/// 
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		/// <param name="text">read-only refernece to the text that will be output'd to console.</param>
+		static inline void writeLine(Variant const & text) {}
+		static inline void wl(Variant const & text) { writeLine(text); }
+
+		/// <summary>
+		/// Output to the console the collection of text, after which adds a new line.
+		/// 
+		/// This function does nothing when _DEBUG is not defined.
+		/// </summary>
+		/// <param name="texts">read-only reference to the collection of text that will be output'd to console.</param>
+		static inline void writeLine(std::initializer_list<Variant> const & texts) {}
+		static inline void wl(std::initializer_list<Variant> const & texts) { writeLine(texts); }
+
+		#endif
+	public: // Public Member Functions
+	public: // Public Static Variables
+	public: // Public Member Variables
+	protected: // Protected Static Functions
+	protected: // Protected Member Functions
+	protected: // Protected Static Variables
+	protected: // Protected Member Variables
+	private: // Private Static Functions
+	private: // Private Member Functions
+	private: // Private Static Variables
+	private: // Private Member Variables
+	};
+	using cout = Console;
+}
